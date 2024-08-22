ec42f522a13bfbfbf5f9a0a0eeb1a8066b020754
AyshBoumann <aysha.boumannn@gmail.com>
2024-08-22 09:03:27 -0300

    aula3

---
+++ aula3.js
@@ -0,0 +1,58 @@
+/*for (let p = 0; p <0; p++) {
+    console.log(" número " + i)
+}
+
+let textinho = "";
+let i = 0;
+while (i < 0) {
+    textinho = " O número é " + i;
+    console.log(textinho)
+    i++
+}
+
+let text = "";
+let a = 0;
+do { 
+    text = " The number is " + a;
+    i++
+    console.log(text)
+}
+while (a < 0);
+
+let listaPalavras = ["figma", "palavra", "java", "galinha"]
+
+for (let o of listaPalavras) {
+    for (let x of o)
+    console.log(x)
+}
+
+
+for (let m = 1; m <= 10; m++) {
+    console.log(`3 + ${m} = ${3 + m}`);
+}
+
+console.log("-----------------");
+
+for (let l = 1; l <= 10; l++) {
+    console.log(`3 * ${l} = ${3 * l}`);
+}
+
+for (let u = 10; u <=20; u++) {
+    if (u==15){
+        break;
+    }
+    console.log(" O número é: " + i)
+}
+
+for (let k = 0; k <=10; k++) {
+    if (k==3){
+        continue;
+    }
+    console.log(" O número é: " + i)
+}*/
+
+for(let f = 0; f <= 15; f++) {
+    if (f % 2 != 0){
+        console.log(` ${f} é impar `)
+    }
+}
